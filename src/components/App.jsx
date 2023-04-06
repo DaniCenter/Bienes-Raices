15357ac409e89361d431b1edf099232b30edef66
Daniel <vilcadaniel53@gmail.com>
2023-04-06 14:32:45 -0500

    about us section with dynamic charts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
+import icono1 from "../resource/icono1.svg";
+import icono2 from "../resource/icono2.svg";
+import icono3 from "../resource/icono3.svg";
 import "../styles/App.css";
 function App() {
   const nav = [
@@ -10,10 +13,31 @@ function App() {
     { url: "contacto.html", titulo: "Contacto" },
   ];
 
+  const cartas = [
+    {
+      imagen: icono1,
+      titulo: "Seguridad",
+      texto:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus velit, tristique vitae nunc sed, tempor cursus dolor. Nullam sodales pretium neque, et faucibus ligula hendrerit non.",
+    },
+    {
+      imagen: icono2,
+      titulo: "Precio",
+      texto:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus velit, tristique vitae nunc sed, tempor cursus dolor. Nullam sodales pretium neque, et faucibus ligula hendrerit non.",
+    },
+    {
+      imagen: icono3,
+      titulo: "A Tiempo",
+      texto:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus velit, tristique vitae nunc sed, tempor cursus dolor. Nullam sodales pretium neque, et faucibus ligula hendrerit non.",
+    },
+  ];
+
   return (
     <>
-      <Header enlaces={nav} />
-      <Main />
+      <Header enlaces={nav} clase={"inicio"} titulo={"Venta de casas y departamentos exclusivos de lujo"} />
+      <Main titulo={"Mas sobre nosotros"} cartas={cartas} />
       <Footer enlaces={nav} />
     </>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,20 @@
-function Main() {
+import "../styles/Principal.css";
+
+function Main({ titulo, cartas }) {
   return (
     <main className="contenedor seccion">
-      <h1>Titulo </h1>
+      <h1>{titulo}</h1>
+      <div className="icono-nosotros">
+        {cartas.map(({ imagen, titulo, texto }) => {
+          return (
+            <div className="icon" key={titulo}>
+              <img src={imagen} alt={titulo} />
+              <h3>{titulo}</h3>
+              <p>{texto}</p>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
