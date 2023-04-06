d917e2b2850584db55b23bc84bbd0f813bcef8cf
Daniel <vilcadaniel53@gmail.com>
2023-04-06 08:16:17 -0500

    Basic structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import imagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    imagemin({
+      exclude: "node_modules/**",
+      verbose: true,
+      // solo tomará imágenes dentro de la carpeta assets
+      include: ["src/resource/**/*.{jpg,png,gif,svg}"],
+      // opciones de configuración de optimización de imágenes
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: true,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+        ],
+      },
+    }),
+  ],
+});
