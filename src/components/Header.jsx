20dbe66000e4ac48148634bb494dc4a7f0ab629b
Daniel <vilcadaniel53@gmail.com>
2023-04-06 14:18:04 -0500

    add dynamic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import logo from "../resource/logo.svg";
 import "../styles/Header.css";
 
-function Header({ enlaces }) {
+function Header({ enlaces, clase, titulo }) {
   return (
-    <header className="header">
+    <header className={`header ${clase ? clase : ""}`}>
       <div className="contenedor contenido-header">
         <div className="barra">
           <a href="/">
@@ -21,7 +21,7 @@ function Header({ enlaces }) {
             })}
           </nav>
         </div>
-        <h1>Venta de casas y departamentos exlucisvos</h1>
+        {titulo ? <h1>{titulo}</h1> : ""}
       </div>
     </header>
   );
