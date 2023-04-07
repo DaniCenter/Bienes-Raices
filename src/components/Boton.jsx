37854432998b2c4c7c05a742647db200f2321db2
Daniel <vilcadaniel53@gmail.com>
2023-04-07 12:52:33 -0500

    add block dinamic

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,6 +1,6 @@
-function Boton({ link, texto, botonColor }) {
+function Boton({ link, texto, botonColor, block }) {
   return (
-    <a href={link} className={`boton ${botonColor ? `boton-${botonColor}` : ""}`}>
+    <a href={link} className={`boton ${botonColor ? `boton-${botonColor}` : ""} ${block ? block : ""}`}>
       {texto}
     </a>
   );
