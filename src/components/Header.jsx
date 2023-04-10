a69d71780be3fbaaf7bb9951bf4d75b93055f0c1
Daniel <vilcadaniel53@gmail.com>
2023-04-10 16:38:26 -0500

    add menu animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import img from "../resource/barras.svg";
 import logo from "../resource/logo.svg";
 import "../styles/Header.css";
 
 function Header({ enlaces, clase, titulo }) {
+  useEffect(() => {
+    document.querySelector(".mobile-menu").addEventListener("click", () => {
+      document.querySelector(".navegacion").classList.toggle("mostrar");
+    });
+  }, []);
+
   return (
     <header className={`header ${clase ? clase : ""}`}>
       <div className="contenedor contenido-header">
@@ -10,6 +17,9 @@ function Header({ enlaces, clase, titulo }) {
           <a href="/">
             <img src={logo} alt="logo" />
           </a>
+          <div className="mobile-menu">
+            <img src={img} alt="barras svg" />
+          </div>
 
           <nav className="navegacion">
             {enlaces.map(({ url, titulo }) => {
